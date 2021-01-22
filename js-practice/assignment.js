1fa16a4bd6da90473552db78821ac4045b77e4bd
Abu Naser Dipu <abunaserdipu0@gmail.com>
2021-01-22 14:40:31 +0600

    add something

--- js-practice/assignment.js
+++ js-practice/assignment.js
@@ -1,12 +1,17 @@
-function kilometerToMeter(kilometer){
+// Calculation kilometerToMeter
+
+function kilometerToMeter(kilometer) {
     var meter = kilometer * 1000;
     return meter;
 }
+
+//Calculation budgetCalculator
 
 function budgetCalculator(watch, phone, laptop) {
     var total = watch * 50 + phone * 100 + laptop * 500;
     return total;
 }
+// Calculation hotelCost
 
 function hotelCost(day) {
     var total = 0;
@@ -28,16 +33,16 @@ function hotelCost(day) {
     }
     return total;
 }
+// Calculation megaFriend
 
-function megaFriend([friendLargeName]){
+function megaFriend([friendLargeName]) {
     var friendLargeName = ["Ikramul", "Zayed", "Iffat"];
     var max = friendLargeName[0];
-for(var i = 0; i < friendLargeName.length; i++){
-    var element = friendLargeName[i];
-    if(element > max){
-        max = element;
-        console.log(max);
+    for (var i = 0; i < friendLargeName.length; i++) {
+        var element = friendLargeName[i];
+        if (element > max) {
+            max = element;
+        }
     }
-}
-// return max;
+    return max;
 }
